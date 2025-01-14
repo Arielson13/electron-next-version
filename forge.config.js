81d71d85804d7024e431aabc0fe85501920d925c
Arielson Sousa Duarte <52675088+Arielson13@users.noreply.github.com>
2025-01-14 16:26:41 -0300

    Tela de login gamer adicionada

--- forge.config.js
+++ forge.config.js
@@ -1,36 +1,36 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-const path = require('path');
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+const path = require("path");
 
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: path.resolve(__dirname, 'assets/icons/app')
+    icon: path.resolve(__dirname, "assets/icons/game"),
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {
-        setupIcon: path.resolve(__dirname, 'assets/icons/app.ico')
+        setupIcon: path.resolve(__dirname, "assets/icons/game.ico"),
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
     // Fuses are used to enable/disable various Electron functionality
--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
+const path = require("path");
 
 // Função para criar a janela principal
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 450,
+    maximizable: false,
+    resizable: false,
+    autoHideMenuBar: true,
     webPreferences: {
-      preload: path.resolve(__dirname, 'preload.js'), // Caminho absoluto para o preload.js
+      preload: path.resolve(__dirname, "preload.js"), // Caminho absoluto para o preload.js
     },
   });
 
-  win.loadFile('index.html');
+  win.setMenuBarVisibility = false;
+  win.loadFile("index.html");
 };
 
 // Evento para alternar o tema escuro/claro
-ipcMain.handle('dark-mode:toggle', () => {
+ipcMain.handle("dark-mode:toggle", () => {
   // Inverte o tema atual
-  nativeTheme.themeSource = nativeTheme.shouldUseDarkColors ? 'light' : 'dark';
+  nativeTheme.themeSource = nativeTheme.shouldUseDarkColors ? "light" : "dark";
   // Retorna o estado atualizado (true para escuro, false para claro)
   return nativeTheme.shouldUseDarkColors;
 });
 
 // Evento para resetar o tema para o padrão do sistema
-ipcMain.handle('dark-mode:system', () => {
+ipcMain.handle("dark-mode:system", () => {
   // Define o tema como o padrão do sistema
-  nativeTheme.themeSource = 'system';
+  nativeTheme.themeSource = "system";
   // Retorna o estado atual (true para escuro, false para claro)
   return nativeTheme.shouldUseDarkColors;
 });
@@ -33,15 +37,15 @@ ipcMain.handle('dark-mode:system', () => {
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
